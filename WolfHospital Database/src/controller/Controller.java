8edfdeaf03b51646e48e9efe70c070f13b995196
jktew <jktew@LAPTOP-2JD6RMNQ>
2017-11-09 14:42:49 -0500

    Started controller

--- WolfHospital Database/src/controller/Controller.java
+++ WolfHospital Database/src/controller/Controller.java
@@ -1,10 +1,25 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Controller {
 
+	private static final String URL = "jdbc:mysql://localhost:3306/wolfhospital";
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "";
+	
+	private static Connection conn;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		try {
+			Class.forName(DRIVER);
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (Exception e) {
+			
+		}
 	}
 
 }
