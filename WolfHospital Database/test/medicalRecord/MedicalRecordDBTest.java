ba64abd3eb521d5fd57fc199da62eee59d061d89
ajfelion <ajfelion@ncsu.edu>
2017-11-15 12:16:20 -0500

    finished medical record test

--- WolfHospital Database/test/medicalRecord/MedicalRecordDBTest.java
+++ WolfHospital Database/test/medicalRecord/MedicalRecordDBTest.java
@@ -1,9 +1,17 @@
 package medicalRecord;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Random;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import controller.Controller;
@@ -17,24 +25,95 @@ public class MedicalRecordDBTest {
 
     private static Connection conn;
 
-    @Test
-    public void testGetById () {
-        fail( "Not yet implemented" );
+    private MedicalRecordDB   db;
+
+    @Before
+    public void setUp () throws Exception {
+        try {
+            Class.forName( DRIVER );
+            conn = DriverManager.getConnection( URL, USERNAME, PASSWORD );
+            db = new MedicalRecordDB( conn );
+        }
+        catch ( final Exception e ) {
+            e.printStackTrace();
+        }
     }
 
+    @SuppressWarnings ( "deprecation" )
     @Test
-    public void testInsert () {
-        fail( "Not yet implemented" );
+    public void testGetById () throws ClassNotFoundException, SQLException {
+
+        MedicalRecord m = null;
+        m = db.getById( 2001 );
+
+        assertNotNull( m );
+        assertEquals( 3001, m.getPatientId() );
+        final Date d1 = new Date( 117, 9, 5 );
+        assertEquals( d1, m.getStartDate() );
+        final Date d2 = new Date( 117, 9, 31 );
+        assertEquals( d2, m.getEndDate() );
+        assertEquals( 1003, m.getDoctorId() );
+        assertEquals( "TB blood test", m.getTestType() );
+        assertEquals( "positive", m.getTestResult() );
+        assertEquals( "antibiotics", m.getPrescription() );
+        assertEquals( "Testing for TB", m.getDiagDetails() );
+        assertEquals( "TB treatment", m.getTreatment() );
+        assertEquals( 50, m.getConsultFee(), 0 );
+        assertEquals( 75, m.getTestFee(), 0 );
+        assertEquals( 199, m.getTreatmentFee(), 0 );
+        assertEquals( 1004, m.getSpecialistId() );
     }
 
+    @SuppressWarnings ( "deprecation" )
     @Test
-    public void testUpdate () {
-        fail( "Not yet implemented" );
+    public void testInsert () throws ClassNotFoundException, SQLException {
+
+        final Date d1 = new Date( 117, 8, 5 );
+        final Date d2 = new Date( 117, 8, 31 );
+        final Random ran = new Random();
+        final int x = ran.nextInt( 9999 );
+        final MedicalRecord m = new MedicalRecord( x, 3001, d1, d2, 1003, "test type", "results", "prescription",
+                "details", "treatment", 1, 2, 3, 1004 );
+
+        assertTrue( db.insert( m ) );
+        final MedicalRecord s2 = db.getById( x );
+        assertEquals( "test type", s2.getTestType() );
     }
 
+    @SuppressWarnings ( "deprecation" )
     @Test
-    public void testDeleteById () {
-        fail( "Not yet implemented" );
+    public void testUpdate () throws ClassNotFoundException, SQLException {
+
+        final Date d1 = new Date( 117, 8, 5 );
+        final Date d2 = new Date( 117, 8, 31 );
+        final Random ran = new Random();
+        final int x = ran.nextInt( 9999 );
+        final MedicalRecord m = new MedicalRecord( x, 3001, d1, d2, 1003, "test type", "results", "prescription",
+                "details", "treatment", 1, 2, 3, 1004 );
+        assertTrue( db.insert( m ) );
+
+        final MedicalRecord m2 = new MedicalRecord( x, 3001, d1, d2, 1004, "test type2", "results2", "prescription2",
+                "details2", "treatment2", 7, 8, 9, 1003 );
+
+        assertTrue( db.update( x, m2 ) );
+        final MedicalRecord m3 = db.getById( x );
+        assertEquals( "test type2", m3.getTestType() );
+    }
+
+    @SuppressWarnings ( "deprecation" )
+    @Test
+    public void testDeleteById () throws ClassNotFoundException, SQLException {
+
+        final Date d1 = new Date( 117, 8, 5 );
+        final Date d2 = new Date( 117, 8, 31 );
+        final Random ran = new Random();
+        final int x = ran.nextInt( 9999 );
+        final MedicalRecord m = new MedicalRecord( x, 3001, d1, d2, 1003, "test type", "results", "prescription",
+                "details", "treatment", 1, 2, 3, 1004 );
+        assertTrue( db.insert( m ) );
+        assertTrue( db.deleteById( x ) );
+        assertNull( db.getById( x ) );
+
     }
 
 }
